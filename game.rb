4abf948f9094d55f9673ec7e6f9d6601c314a9ac
rhermes1 <rocco-hermes@web.de>
2014-05-14 15:35:35 +0200

    2048: game
    write random spawn for numbers
    write first emthod for move

--- game.rb
+++ game.rb
@@ -1,16 +1,55 @@
 #!/usr/bin/env ruby
 class Field
-  attr_accessor :field
+  attr_accessor :field_columns, :up, :right, :down, :left
 
-  def create_game_field
-    self.field = Array.new(4){ Array.new(4) {"2"}}
+  def initialize
+    self.field_columns = Array.new(4){ Array.new(4){nil}}
+    self.up, self.right, self.down, self.left = "w", "d", "s", "a"
+  end
+
+  def spawn_random_number
+    numbers = [2, 2, 4, 2, 4, 2, 2].shuffle
+    spawn_numbers = [1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0].shuffle
+    x_rand, y_rand = spawn_numbers[rand(12)], spawn_numbers[rand(12)]
+    if self.field_columns[x_rand][y_rand] == nil
+      self.field_columns[x_rand][y_rand] = numbers[rand(5)]
+    else
+      spawn_random_number
+    end
+  end
+
+  def move_direction
+  next_step = gets.chomp
+    case next_step
+      when self.up
+        puts "up"
+      when self.right
+        puts "right"
+      when self.down
+        puts "down"
+      when self.left
+        puts "left"
+      else
+        system("clear")
+        puts self
+        puts "Nothing defined, repeat:"
+        move_direction
+    end
   end
 
   def to_s
-    self.field.map { |row| row.join }.join("\n")
+    self.field_columns.map { |row| row.join }.join("\n")
   end
+
 end
 
-field = Field.new
-field.create_game_field
-puts field
+@field = Field.new
+i = 0
+while i < 8 do
+  @field.spawn_random_number
+  system("clear")
+  puts @field
+  print "Input:"
+  @field.move_direction
+  i += 1
+end
