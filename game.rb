6643e7a0f260fb737eedccbe050253069a586d39
rhermes1 <rocco-hermes@web.de>
2014-05-26 10:29:05 +0200

    Direction right and no error msg when no number can spawn

--- game.rb
+++ game.rb
@@ -7,14 +7,25 @@ class Field
     self.up, self.right, self.down, self.left = "w", "d", "s", "a"
   end
 
-  def spawn_random_number
+  def output
+    system("clear")
+    puts self
+    puts "(#{self.up})up|(#{self.left})left|"+
+      "(#{self.down})down|(#{self.right})right"
+    print "Input:"
+  end
+
+  def spawn_random_number(count)
     numbers = [2, 2, 4, 2, 4, 2, 2].shuffle
     spawn_numbers = [1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0].shuffle
     x_rand, y_rand = spawn_numbers[rand(12)], spawn_numbers[rand(12)]
     if self.field_columns[x_rand][y_rand] == " "
+      count = 0
       self.field_columns[x_rand][y_rand] = numbers[rand(5)]
     else
-      spawn_random_number
+      if count < 16
+        spawn_random_number(count+1)
+      end
     end
   end
 
@@ -31,17 +42,16 @@ class Field
       check_array[x], check_array[y] = check_array[y], " "
       return {1 => "redo", 2 => check_array}
     end
+    return {1 => "break", 2 => check_array}
   end
 
   def check_neigbour_number(check_array, x, x_dir)
-    puts "-------"
-    p check_array
-    puts "-------"
     z, i = x_dir, 0
     while i < check_array.length do
       y = x
       while true do
         y += x_dir
+        break if y < 0
         wh_nex = compare_fields(check_array, x, y)
         if wh_nex[1] == "redo"
           check_array = wh_nex[2]
@@ -55,17 +65,16 @@ class Field
       z += x_dir
       i += 1
     end
-    p check_array
+    return check_array
   end
 
   def move_direction(start)
     self.field_columns.each do |row|
-      p row
       case start
         when("left")
           check_neigbour_number(row, 0, 1)
         when("right")
-          check_neigbour_number(row.reverse, 0, 1)
+          check_neigbour_number(row, row.length, -1)
       end
     end
   end
@@ -82,22 +91,28 @@ class Field
       when self.left
         move_direction("left")
       else
-        puts self + "Nothing defined, repeat:"
+        output
         choose_direction
     end
   end
 
+  def column
+    "-+" * self.field_columns.length
+  end
+
   def to_s
-    self.field_columns.map { |row| row.join("|") }.join("|\n") + "|"
+    column + "\n" + self.field_columns.map { |row| row.join("+")}.join("|\n") +
+      "|\n" + column
   end
 end
 
+
 @field = Field.new
-i = 0
-while i < 100 do
-  @field.spawn_random_number
-  puts @field
-  print "Input:"
+i = true
+while i == true do
+  i = true
+  @field.spawn_random_number(0)
+  @field.output
+  puts i
   @field.choose_direction
-  i += 1
 end
